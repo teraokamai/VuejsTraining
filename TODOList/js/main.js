e93c431ba0cc5e1d5b084cb11f0814d86a901cd8
m-teraoka-mac <mai.teraoka2525@gmail.com>
2020-08-22 20:21:14 +0900

    TODOListアプリケーション作成

---
+++ TODOList/js/main.js
@@ -0,0 +1,23 @@
+new Vue ({
+  el: '#app',
+  data: {
+    newItem: '',
+    todos: []
+  },
+  methods: {
+    addItem: function(event){
+      if(this.newItem == '') return;
+
+      var todo = {
+        item: this.newItem,
+        isDone: false
+      };
+      this.todos.push(todo);
+      this.newItem =  '';
+    },
+    deleteItem: function(index){
+      this.todos.splice(index,1)
+    }
+
+  }
+})
